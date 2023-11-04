72ab98e9c2e6d36b994a7b929fd4532108ec2262
MarkAyl5 <marcosda05@gmail.com>
2023-11-04 16:00:31 -0300

    Ejercicio 1

---
+++ Ejercicio1.java
@@ -0,0 +1,108 @@
+package semana1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		
+		int numeroInicio = 5;
+		int numeroFinal = 14;
+		int numeroActual = numeroInicio;
+		boolean mostrarImpar = true;		
+		
+		
+		
+		
+		
+	String Ejercicio1 = "A";
+	
+	
+		
+		while(numeroActual <= numeroFinal) {	
+				System.out.println(numeroActual + ", ");
+				numeroActual++;
+		}
+		
+		
+		
+		
+	Ejercicio1 = "B";
+		
+			
+			
+//			while(numeroActual <= numeroFinal) {
+//				
+//					if(numeroActual%2 == 0) {
+//					
+//						System.out.println(numeroActual + ", ");
+//				
+//					}
+//				
+//				numeroActual++;
+//			}
+//		
+		
+		
+	Ejercicio1 = "C";
+		
+//	while(numeroActual <= numeroFinal) {
+//		
+//			if(mostrarImpar){
+//			
+//				if(numeroActual%2 != 0) {
+//			
+//					System.out.println(numeroActual + ", ");
+//		}
+//		}
+//		
+//	numeroActual++;
+//
+//
+//	}	
+		
+		
+		
+	Ejercicio1 ="D";
+//		
+//		for (int i = numeroFinal+1; i > numeroActual; i--) {
+//				if(i%2 ==0) {
+//					System.out.println(i);
+//				}
+//		}
+//		
+//		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
